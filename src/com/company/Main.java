95e544652a6687093b4080d7fd1b665ae65c435c
BT_1N3_09@BKTECH.LOCAL <BT_1N3_09@BKTECH.LOCAL>
2018-09-24 09:51:19 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Scanner name = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String reply = name.nextLine();
+        System.out.println("Nice to meet you " + reply + "!");
+        name.close();
+    }
+}
